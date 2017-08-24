e690d4c85278992839e98e1533351c9ef4879090
takvol <takvol@users.noreply.github.com>
2017-08-24 12:39:05 +0300

    Update popup.js

--- assets/popup.js
+++ assets/popup.js
@@ -3,3 +3,9 @@ var t = TrelloPowerUp.iframe();
 t.render(function(){
   t.sizeTo('#popup').done();
 });
+
+document.querySelector('button').addEventListener('click', function(event){
+  fetch('https://cyplarix.com').then(function(result){
+    console.log(result);
+  });
+});

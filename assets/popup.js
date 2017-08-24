ec23629fab832c7c4647664f81ada8b83067c979
takvol <takvol@users.noreply.github.com>
2017-08-24 21:17:42 +0300

    Update popup.js

--- assets/popup.js
+++ assets/popup.js
@@ -5,7 +5,8 @@ t.render(function(){
 });
 
 document.querySelector('button').addEventListener('click', function(event){
-  fetch('https://cyplarix.com').then(function(result){
+  key = '67830660e9ef8b444f8c8ecfed93345873974fcf66bedc44331f8d26b20e49fd';
+  fetch('https://script.google.com/macros/s/AKfycbz62Z78Wb1iE2awWeDuV96cf1hSCBw9WfnPh1o89hp4u6M96bE/exec?'+key).then(function(result){
     console.log(result);
   });
 });

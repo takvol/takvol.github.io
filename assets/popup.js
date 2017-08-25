9c2955fed37d55e0fa746f655ea8efb6eceec0e4
takvol <takvol@users.noreply.github.com>
2017-08-25 10:47:33 +0300

    Update popup.js

--- assets/popup.js
+++ assets/popup.js
@@ -6,11 +6,17 @@ t.render(function(){
 
 document.querySelector('button').addEventListener('click', function(event){
   key = '67830660e9ef8b444f8c8ecfed93345873974fcf66bedc44331f8d26b20e49fd';
-  card = t.card('id','name','url');
-  board = t.board('id');
-  fetch('https://script.google.com/macros/s/AKfycbz62Z78Wb1iE2awWeDuV96cf1hSCBw9WfnPh1o89hp4u6M96bE/exec?key=' + key
-       + '&card_id=' + card.id + '&card_name=' + card.name + '&card_url=' + card.url + '&board_id=' + board.id)
-        .then(function(result){
+  url = 'https://script.google.com/macros/s/AKfycbz62Z78Wb1iE2awWeDuV96cf1hSCBw9WfnPh1o89hp4u6M96bE/exec?key=' + key;
+  
+  t.card('id','name','url').then(function(card){
+    url += '&card_id=' + card.id + '&card_name=' + card.name + '&card_url=' + card.url;
+  });
+  
+  t.board('id').then(function(board){
+    url += '&board_id=' + board.id;
+  });
+  
+  fetch(url).then(function(result){
     console.log(result);
   });
 });

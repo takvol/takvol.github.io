20d5b90e0fc4f9b0c501e4c1de8071491729a761
takvol <takvol@users.noreply.github.com>
2017-08-25 10:38:41 +0300

    Update popup.js

--- assets/popup.js
+++ assets/popup.js
@@ -6,7 +6,11 @@ t.render(function(){
 
 document.querySelector('button').addEventListener('click', function(event){
   key = '67830660e9ef8b444f8c8ecfed93345873974fcf66bedc44331f8d26b20e49fd';
-  fetch('https://script.google.com/macros/s/AKfycbz62Z78Wb1iE2awWeDuV96cf1hSCBw9WfnPh1o89hp4u6M96bE/exec?'+key).then(function(result){
+  card = t.card('id','name','url');
+  board = t.board('id');
+  fetch('https://script.google.com/macros/s/AKfycbz62Z78Wb1iE2awWeDuV96cf1hSCBw9WfnPh1o89hp4u6M96bE/exec?key=' + key
+       + '&card_id=' + card.id + '&card_name=' + card.name + '&card_url=' + card.url + '&board_id=' + board.id)
+        .then(function(result){
     console.log(result);
   });
 });
